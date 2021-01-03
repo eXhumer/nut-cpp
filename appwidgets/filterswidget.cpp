2d881bded5c217d623a25bf4bdaabb71f1862899
eXhumer <exhumer@member.fsf.org>
2021-01-03 02:16:41 -0700

    Add Filter Types Checkboxes

--- appwidgets/filterswidget.cpp
+++ appwidgets/filterswidget.cpp
@@ -1,6 +1,7 @@
 #include "filterswidget.h"
 #include <QBoxLayout>
 #include <QGroupBox>
+#include <QCheckBox>
 
 FiltersWidget::FiltersWidget(QWidget *parent)
     : QWidget(parent)
@@ -9,5 +10,14 @@ FiltersWidget::FiltersWidget(QWidget *parent)
     auto typesGroup = new QGroupBox(tr("tabs.filters.types"));
     auto regionsGroup = new QGroupBox(tr("tabs.filters.region"));
     this->layout()->addWidget(typesGroup);
+    typesGroup->setLayout(new QHBoxLayout);
+    auto typesLayout = static_cast<QHBoxLayout*>(typesGroup->layout());
+    typesLayout->addWidget(new QCheckBox(tr("tabs.filters.types.base")));
+    typesLayout->addStretch();
+    typesLayout->addWidget(new QCheckBox(tr("tabs.filters.types.dlc")));
+    typesLayout->addStretch();
+    typesLayout->addWidget(new QCheckBox(tr("tabs.filters.types.update")));
+    typesLayout->addStretch();
+    typesLayout->addWidget(new QCheckBox(tr("tabs.filters.types.demo")));
     this->layout()->addWidget(regionsGroup);
 }
